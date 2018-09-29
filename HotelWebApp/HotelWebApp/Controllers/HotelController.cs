ef68396a878afe0778001891677ab6bfaff25a2a
Pallvi Goel <pgoel@tavisca.com>
2018-09-29 19:20:08 +0530

    Added try catch functanility

--- HotelWebApp/HotelWebApp/Controllers/HotelController.cs
+++ HotelWebApp/HotelWebApp/Controllers/HotelController.cs
@@ -12,31 +12,61 @@ namespace HotelWebApp.Controllers
 {
     public class HotelController : ApiController
     {
-        AerospikeClient aerospikeClient = new AerospikeClient("18.235.70.103", 3000);
-        string nameSpace = "AirEngine";
-        string setName = "Pallvi3";
-        //List<Record>
+        string nameSpace = null;
+        string setName = null;
+        AerospikeClient aerospikeClient = null;
+        public void Connect()
+        {
+            try
+            {
+                aerospikeClient = new AerospikeClient("18.235.70.103", 3000);
+                nameSpace = "AirEngine";
+                setName = "Pallvi3";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         [System.Web.Http.HttpPost]
-   
+
         public List<Record> GetMultipleData([FromBody]JObject jsonFormatInput)
         {
             List<Record> output = new List<Record>();
             for (int i = 1; i <= jsonFormatInput.Count; i++)
             {
                 string id = jsonFormatInput.GetValue("id" + i).ToString();
-                var key = new Key(nameSpace, setName, id.ToString());
-                Record objects = aerospikeClient.Get(new WritePolicy(), key);
-                output.Add(objects);
+                try
+                {
+                    var key = new Key(nameSpace, setName, id.ToString());
+                    Record objects = aerospikeClient.Get(new WritePolicy(), key);
+                    output.Add(objects);
+                }
+                catch (Exception ex)
+                {
+
+
+                    throw ex;
+                }
+
             }
+
             return output;
         }
         [System.Web.Http.HttpPut]
         [System.Web.Http.Route("put/{id}")]
         public void UpdateDataByID(long id, [FromBody]string updatedYear)
-        {  Bin newBin = new Bin("year", updatedYear);
+        {
+            Bin newBin = new Bin("year", updatedYear);
             var key = new Key(nameSpace, setName, id.ToString());
-            aerospikeClient.Put(new WritePolicy(), key, newBin);
-       
+            try
+            {
+                aerospikeClient.Put(new WritePolicy(), key, newBin);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         [System.Web.Http.HttpDelete]
@@ -44,7 +74,14 @@ namespace HotelWebApp.Controllers
         public void DeleteData(long id)
         {
             var key = new Key(nameSpace, setName, id.ToString());
-            aerospikeClient.Delete(new WritePolicy(), key);
+            try
+            {
+                aerospikeClient.Delete(new WritePolicy(), key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 
